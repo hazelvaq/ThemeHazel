7b0c7054ea057c2456e6d7215bd6dceaa6a636cf
hazelvaq <hvaquero98@gmail.com>
2023-08-25 10:59:01 -0700

    Initial commit

---
+++ R/theme_eight.R
@@ -0,0 +1,6 @@
+theme_eight <- function(){
+  theme(plot.background = element_rect(fill = "magenta"),
+        panel.background = element_rect(fill = "gray"),
+        axis.text = element_text(color = "yellow"),
+        panel.grid = element_line(color = "cyan"))
+}
